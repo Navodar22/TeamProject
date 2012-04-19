52667ed9c496f32fa609c9d3f262c529ecca0fc5
Milanista <karmikike@ovi.com>
2012-04-19 20:55:34 +0200

    statistika upravena

--- app/presenters/StatisticsPresenter.php
+++ app/presenters/StatisticsPresenter.php
@@ -146,33 +146,35 @@ class StatisticsPresenter extends BaseLPresenter
 		
                 
 	public function createComponentDataGridProjects() {
-
+	    $today = date("Y-m-d");
 		if(empty($this->dateRange)) {
-			$source = $this->db->table('project');
-		} else {
-			$source = $this->db->table('project')
-					->where('project.start >= ?', $this->dateRange->from)
-					->where('project.end <= ?', $this->dateRange->to);
+			$source = $this->db->table('project')->where('end < ?',$today );
+		}
+		else {
+			$source = $this->db->table('project')->where('end < ?',$today );
 		}
 
 		if($source->count('*') <= 0) {
 			
 			$dg = new DataGrid();
 			$dg->setDataSource($source);
-
+	
 			$dg->template->empty = true;
 			return $dg;
+			
+
 		}
 		
         $dg = new DataGrid();
+		
         $dg->setDataSource($source);
-	
-       
+		//$dg['end']->addDefaultSorting('asc');
+        
         $dg->addColumn('id', 'No.')->setStyle('width: 50px');
         $dg->addColumn('name', 'Name')->setStyle('width: 50px');
-        $dg->addColumn('start', 'Začiatok')->setStyle('width: 50px');    
-        $dg->addColumn('end', 'Koniec')->setStyle('width: 50px');    
-	
+        $dg->addDateColumn('start', 'Začiatok','%d.%m.%Y')->setStyle('width: 50px');    
+        $dg->addDateColumn('end', 'Koniec','%d.%m.%Y')->setStyle('width: 50px');
+
 		
 	return $dg;
 	}
